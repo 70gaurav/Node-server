00c848291837269fc769e8a7a98d3f4c46fa7aa5
70gaurav <gk17265@gmail.com>
2023-04-20 10:35:42 +0530

    added put and delete

--- Server.js
+++ Server.js
@@ -1,24 +1,45 @@
 import http from "http"
 
-const names = ["gaurav" , "saurav"]
-const server = http.createServer((req,res) => {
-if(req.method === "GET"){
-    res.writeHead(200,{"Content-Type" : "application/json"})
+const names = ["gaurav", "saurav"]
+const server = http.createServer((req, res) => {
+  if (req.method === "GET") {
+    res.writeHead(200, { "Content-Type": "application/json" })
     res.end(JSON.stringify(names))
-}
-
-else if(req.method === "POST"){
-    res.writeHead(200,{"Content-Type" : "application/json"})
-    req.on("data" , (data) =>{
-        const dataString = data.toString()
-        if(dataString.includes(",")){
-            const newData = dataString.split(",")
-            names.push(...newData)
-        }
-        else{
-            names.push(dataString)
-        }
+  } else if (req.method === "POST") {
+    res.writeHead(200, { "Content-Type": "application/json" })
+    req.on("data", (data) => {
+      const dataString = data.toString()
+      if (dataString.includes(",")) {
+        const newData = dataString.split(",")
+        names.push(...newData)
+      } else {
+        names.push(dataString)
+      }
     })
+    req.on("end", () => {
+      res.end(JSON.stringify(names))
+    })
+  } else if (req.method === "PUT") {
+    res.writeHead(200, { "Content-Type": "application/json" })
+    req.on("data", (data) => {
+      const dataString = data.toString()
+      if (dataString.includes(",")) {
+        const newData = dataString.split(",")
+        names.splice(0, names.length, ...newData)
+      } else {
+        names.splice(0, names.length, dataString)
+      }
+    })
+    req.on("end", () => {
+      res.end(JSON.stringify(names))
+    })
+  } else if (req.method === "DELETE") {
+    res.writeHead(200, { "Content-Type": "application/json" })
+    names.splice(0, names.length)
     res.end(JSON.stringify(names))
-}
-}).listen(3000)
+  }
+})
+
+server.listen(3000,() => {
+    console.log("Server started at port 3000")
+})
