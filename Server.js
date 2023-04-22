38e6b9640c74a7caf48a0b7ee5bbdcbd33cecd81
70gaurav <gk17265@gmail.com>
2023-04-22 13:18:31 +0530

    patch commit

--- Server.js
+++ Server.js
@@ -1,11 +1,14 @@
 import http from "http"
 
 const names = ["gaurav", "saurav"]
+
 const server = http.createServer((req, res) => {
   if (req.method === "GET") {
     res.writeHead(200, { "Content-Type": "application/json" })
     res.end(JSON.stringify(names))
-  } else if (req.method === "POST") {
+  } 
+  
+  else if (req.method === "POST") {
     res.writeHead(200, { "Content-Type": "application/json" })
     req.on("data", (data) => {
       const dataString = data.toString()
@@ -19,7 +22,9 @@ const server = http.createServer((req, res) => {
     req.on("end", () => {
       res.end(JSON.stringify(names))
     })
-  } else if (req.method === "PUT") {
+  }
+  
+  else if (req.method === "PUT") {
     res.writeHead(200, { "Content-Type": "application/json" })
     req.on("data", (data) => {
       const dataString = data.toString()
@@ -33,13 +38,39 @@ const server = http.createServer((req, res) => {
     req.on("end", () => {
       res.end(JSON.stringify(names))
     })
-  } else if (req.method === "DELETE") {
+  }
+  
+  else if (req.method === "DELETE") {
     res.writeHead(200, { "Content-Type": "application/json" })
     names.splice(0, names.length)
     res.end(JSON.stringify(names))
   }
+
+  else if(req.method === "PATCH"){
+    res.writeHead(200, {"Content-Type" : "application/json"})
+    req.on("data" , (data) => {
+      const newData = data.toString()
+      if(newData.includes(",")){
+        const split = newData.split(",")
+        split.forEach((name, index) => {
+          if(names[index]){
+            names[index] = name
+          }
+          else{
+            names.push(name)
+          }
+        })
+      }
+      else{
+        names.splice(0,1,newData)
+      }
+    })
+    req.on("end" , () => {
+      res.end(JSON.stringify(names))
+    })
+  }
 })
 
-server.listen(3000,() => {
-    console.log("Server started at port 3000")
+server.listen(3000, () => {
+  console.log("Server started at port 3000")
 })
